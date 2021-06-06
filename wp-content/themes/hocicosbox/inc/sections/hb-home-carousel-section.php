be5eb1c74694b9e913f60c9455d32d3fe7da9c37
Isabel Fernández <ifernandez@qualita.es>
2021-06-06 12:20:52 +0200

    Carousel V.1

--- wp-content/themes/hocicosbox/inc/sections/hb-home-carousel-section.php
+++ wp-content/themes/hocicosbox/inc/sections/hb-home-carousel-section.php
@@ -1,16 +1,23 @@
 <div class="container-fluid test">
     <div class="row">
-        <div class="col">
+        <div class="col carousel-home">
             <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                </ol>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
                         <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>'/assets/images/carousel-test/header-hocicosbox.png'" alt="">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>TEST</h5>
-                            <p>TEST INFO</p>
+                        <div class="carousel-caption d-none d-md-block home-caption-carousel">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-6"></div>
+                                    <div class="col-6">
+                                        <h4>SOLO ESTA SEMANA…</h4>
+                                        <h5>¡Dtos de hasta el 20%
+                                            para celebrar nuestra
+                                            nueva web!</h5>
+                                        <button class="carousel-shop-button">IR A LA TIENDA</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
