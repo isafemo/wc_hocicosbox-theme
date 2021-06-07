beab2aed3b4b5462c6641036c8ee044c771f7629
Isabel Fernández <ifernandez@qualita.es>
2021-06-07 06:26:26 +0200

    Add fancyLab and delete other themes

--- wp-content/themes/imperial-child/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-add_action( 'wp_enqueue_scripts', 'imperial_child_enqueue_styles' );
-function imperial_child_enqueue_styles() {
-	wp_enqueue_style( 'imperial-parent-style', get_template_directory_uri() . '/style.css', 1000 );
-
-}
